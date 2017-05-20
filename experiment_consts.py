b8bdc55e46dbfc7c4ab1fc0566c03fd122cc5902
Moshe Cohen <moshec40@gmail.com>
2017-05-20 22:43:29 +0300

    * config consts for mario experiment * add logs folder to the ignore list

--- experiment_consts.py
+++ experiment_consts.py
@@ -18,7 +18,7 @@ class ExperimentConsts:
     EXPERIMENT_BASE_BRANCH_NAME_FORMAT = '{base_branch_name}_{experiment_type}'
     EXPERIMENT_USER_BRANCH_NAME_FORMAT = '{user_name}_{experiment_type}'
 
-    EXPERIMENT_FOLDER_NAME = 'predator_experiment_3'
+    EXPERIMENT_FOLDER_NAME = 'mario_experiment'
 
     WORKING_DIRECTORY_PATH = os.path.join(os.path.dirname(__file__), '..', EXPERIMENT_FOLDER_NAME)
 
@@ -28,8 +28,8 @@ class ExperimentConsts:
     EXPERIMENT_LOGS_DIR_PATH = os.path.join(WORKING_DIRECTORY_PATH, 'logs')
     EXPERIMENT_GRAPHS_DIR_PATH = os.path.join(WORKING_DIRECTORY_PATH, 'graphs')
 
-    REMOTE_STORAGE_PATH = r'c:\exp\products'
+    REMOTE_STORAGE_PATH = r'c:\exp\products\mario'
     REMOTE_USER_STORAGE_PATH = os.path.join(REMOTE_STORAGE_PATH, '{user_id}', '{experiment_type}')
 
-    SCRIPTS_BRANCH_NAME = 'single_experiment'
+    SCRIPTS_BRANCH_NAME = 'mario_experiment_scripts'
     SCRIPTS_WORKING_DIRECTORY = os.path.dirname(__file__)
