7c6246cee948f6727db3806d699d23b36f659cbf
moshiky <moshec40@gmail.com>
2017-08-07 17:52:51 +0300

    * implement new logs parsing

--- score_generator.py
+++ score_generator.py
@@ -1,10 +1,10 @@
 
 import os
+import sys
 import csv
 from datetime import datetime
 from logger import Logger
 from evaluation_consts import EvaluationConsts
-from score_generator_consts import ScoreGeneratorConsts
 
 
 class ScoreGenerator:
@@ -12,164 +12,74 @@ class ScoreGenerator:
     def __init__(self, p_logger):
         self.__logger = p_logger
 
-    def __parse_all_lines(self, log_lines):
-        # create result dict
-        result_dict = dict()
+    def __parse_log_file_lines(self, log_lines):
+        # initiate variables and counters
+        evaluation_mean_results_sum = list()
+        evaluation_total_time = 0
+        evaluation_start_time = None
+        evaluation_section_id = 0
+        train_end_time = None
+        train_start_time = None
+        epoch_id = 0
 
-        # experiment round id
-        experiment_round_id = -1
-        last_ep_id = -1
-        logging_stage = None
-
-        # parse lines
-        for line in log_lines:
-
-            if line[0] == '[' and line[20] == ']':  # regular log line
-                line_parts = line.split(' >> ')
-                line_time = line_parts[0]
-                line_content = line_parts[1]
-
-                if line_content in [
-                    'Logger created',
-                    'Abstraction',
-                    'Similarities',
-                    'RewardShaping',
-                    'SimilaritiesOnRewardShaping'
-                ]:
-                    # line not important and could be ignored
-                    continue
-
-                elif line_content.startswith('=== Experiment #'):
-                    # line declares that new experiment round is started
-                    experiment_round_id = (line_content.split('#')[1]).split(' ')[0]
-                    result_dict[experiment_round_id] = \
-                        {
-                            'train': list(),
-                            'eval': list()
-                        }
-                    last_ep_id = -1
-                    logging_stage = 'train'
-
-                elif line_content.startswith('ex') and line_content.find('ep') > 0 \
-                        and line_content.find('mean:') > 0:
-                    # train score log line
-
-                    ex_id = line_content[2:].split('ep')[0]
-                    if str(ex_id) != str(experiment_round_id):
-                        raise Exception(
-                            'wrong log file format. ex id mismatch: '
-                            'declared {experiment_round_id} but line contains {ex_id}'.format(
-                                experiment_round_id=experiment_round_id, ex_id=ex_id
-                            )
-                        )
-
-                    ep_id = (line_content.split('ep')[1]).split(' ')[0]
-                    if int(last_ep_id) + 100 != int(ep_id):
-                        raise Exception(
-                            'wrong log file format. eps not ordered correctly: '
-                            'last is {last_ep_id} but current is {ep_id}'.format(
-                                last_ep_id=last_ep_id, ep_id=ep_id
-                            )
-                        )
-                    last_ep_id = ep_id
-
-                    mean_score = line_content.split(' ')[2]
-                    result_dict[logging_stage].append(mean_score)
-
-                elif line_content.startswith('ex') and line_content.find('eval_ep') > 0:
-                    # evaluation session score log line
-
-                    ex_id = line_content[2:].split('eval')[0]
-                    if str(ex_id) != str(experiment_round_id):
-                        raise Exception(
-                            'wrong log file format. eval ex id mismatch: '
-                            'declared {experiment_round_id} but line contains {ex_id}'.format(
-                                experiment_round_id=experiment_round_id, ex_id=ex_id
-                            )
-                        )
-
-                    ep_id = (line_content.split('eval_ep')[1]).split(' ')[0].replace(':', '')
-                    if str(ep_id) == '0':
-                        logging_stage = 'eval'
-                        last_ep_id = -1
-
-                    if int(last_ep_id) + 1 != int(ep_id):
-                        raise Exception(
-                            'wrong log file format. eval eps not ordered correctly: '
-                            'last is {last_ep_id} but current is {ep_id}'.format(
-                                last_ep_id=last_ep_id, ep_id=ep_id
-                            )
-                        )
-                    last_ep_id = ep_id
-
-                    mean_score = line_content.split(' ')[1]
-                    result_dict[ex_id][logging_stage].append(mean_score)
-
-        return result_dict
-
-    def __parse_selected_lines(self, log_lines):
-        # create result lists
-        eval_results = list()
-        train_episodes_means = None
-
-        last_time = None
-        in_eval = False
-
-        train_total_seconds = 0
-        eval_total_seconds = 0
-
+        # parse log lines
         time_format = '[%d/%m/%Y %H:%M:%S]'
-
-        # iterate log lines and parse lines
         for line in log_lines:
 
-            if line.find('=== Experiment #') > 0:  # first line of train section
-                time_part = line.split(' >> ')[0]
-                curr_time = datetime.strptime(time_part, time_format)
-                if last_time is not None:
-                    eval_total_seconds += (curr_time - last_time).seconds
-                    in_eval = False
-                last_time = curr_time
+            if line.find('=== Experiment #') > 0:  # first line of train epoch
+                log_line_time = datetime.strptime(line.split(' >> ')[0], time_format)
+                if line.find(' #0 ') > 0:     # train start
+                    train_start_time = log_line_time
+                epoch_id += 1
+                evaluation_section_id = 0
 
-            elif line.find('eval_ep') > 0 and not in_eval:  # line of evaluation section
-                time_part = line.split(' >> ')[0]
-                curr_time = datetime.strptime(time_part, time_format)
-                train_total_seconds += (curr_time - last_time).seconds
-                last_time = curr_time
-                in_eval = True
+            elif line.find('-- Evaluation --') > 0:
+                evaluation_start_time = datetime.strptime(line.split(' >> ')[0], time_format)
 
-            elif line.find('ex_eval_mean:') > 0:    # evaluation section mean result
-                eval_results.append(float(line.split('ex_eval_mean:')[1]))
+            elif line.find('evaluation results:') > 0:
+                evaluation_end_time = datetime.strptime(line.split(' >> ')[0], time_format)
+                evaluation_total_time += (evaluation_end_time - evaluation_start_time).seconds + 0.5
 
-            elif line.find('Train episodes mean:') > 0:
-                if train_episodes_means is not None:
-                    raise Exception('duplicated train episodes list')
+            elif line.find('mean result:') > 0:
+                evaluation_mean_result = float(line.split('mean result: ')[1])
+                if epoch_id == 0 or len(evaluation_mean_results_sum) == evaluation_section_id:
+                    evaluation_mean_results_sum.append((evaluation_mean_result, 1))
+                else:
+                    evaluation_mean_results_sum[evaluation_section_id] = \
+                        (
+                            evaluation_mean_results_sum[evaluation_section_id][0] + evaluation_mean_result,
+                            evaluation_mean_results_sum[evaluation_section_id][1] + 1
+                        )
+                evaluation_section_id += 1
 
-                line_part = line.split('Train episodes mean:')[1:][0]
-                list_text = line_part.replace('[', '').replace(']', '')
-                train_episodes_means = list_text.split(', ')
-                train_episodes_means = map(float, train_episodes_means)
+            elif line.find('total time:') > 0:
+                train_end_time = datetime.strptime(line.split(' >> ')[0], time_format)
 
+        # calculate train duration
+        train_duration = (train_end_time - train_start_time).seconds - evaluation_total_time
+
+        # calculate mean evaluation results
+        evaluation_mean_results = [float(x[0])/x[1] for x in evaluation_mean_results_sum]
+
+        # return results
         return {
-            'train': train_episodes_means,
-            'train_time': train_total_seconds,
-            'eval': sum(eval_results) / len(eval_results),
-            'eval_time': eval_total_seconds
+            'evaluation_mean_results': evaluation_mean_results,
+            'evaluation_mean_duration': float(evaluation_total_time) / epoch_id,
+            'train_mean_duration': float(train_duration) / epoch_id
         }
 
-    def generate_users_score(self):
+    def generate_users_score(self, results_folder_path):
         self.__logger.log("begin score generation")
 
         # read id list
-        current_folder_path = os.path.dirname(__file__)
-        user_ids = os.listdir(
-            os.path.join(current_folder_path, '..', ScoreGeneratorConsts.RESULTS_FOLDER_NAME)
-        )
+        user_ids = os.listdir(results_folder_path)
         self.__logger.log("running {num_ids} ids:\n{ids}".format(num_ids=len(user_ids), ids='\n'.join(user_ids)))
 
+        # build the complete experiment list
         exp_list = list(EvaluationConsts.EXPERIMENT_TYPE_KEYS.keys())
         exp_list.append('similarities_on_reward_shaping')
 
+        max_evaluation_sections_number = 0
         raw_info_dict = dict()
         for uid in user_ids:
 
@@ -184,13 +94,7 @@ class ScoreGenerator:
                     'calculating score of {uid} at {experiment_type}'.format(uid=uid, experiment_type=experiment_type)
                 )
 
-                user_log_file_path = \
-                    os.path.join(
-                        current_folder_path, '..',
-                        ScoreGeneratorConsts.RESULTS_FOLDER_NAME,
-                        uid,
-                        experiment_type + '.log'
-                    )
+                user_log_file_path = os.path.join(results_folder_path, uid, experiment_type + '.log')
 
                 if not os.path.exists(user_log_file_path):
                     self.__logger.error(
@@ -209,9 +113,9 @@ class ScoreGenerator:
                 # remove header
                 log_file_lines = log_file_lines[4:]
 
+                # parse lines
                 try:
-                    # raw_info_dict[uid][experiment_type] = self.__parse_all_lines(log_file_lines)
-                    raw_info_dict[uid][experiment_type] = self.__parse_selected_lines(log_file_lines)
+                    raw_info_dict[uid][experiment_type] = self.__parse_log_file_lines(log_file_lines)
 
                 except Exception, ex:
                     self.__logger.error('log file parsing failed. uid={uid} exp_type={exp_type} ex={ex}'.format(
@@ -219,39 +123,29 @@ class ScoreGenerator:
                     ))
                     return None
 
-        self.__logger.log('extracting learning curve info')
-        split_keys = list()
+                # compare evaluation results list to max length
+                if len(raw_info_dict[uid][experiment_type]['evaluation_mean_results']) > max_evaluation_sections_number:
+                    max_evaluation_sections_number = len(raw_info_dict[uid][experiment_type]['evaluation_mean_results'])
+
+        self.__logger.log('preparing for csv')
         for uid in user_ids:
-
             for experiment_type in exp_list:
-                user_train_results = raw_info_dict[uid][experiment_type]['train']
-                raw_info_dict[uid][experiment_type]['train_mean'] = sum(user_train_results) / len(user_train_results)
+                evaluation_results = raw_info_dict[uid][experiment_type]['evaluation_mean_results']
+                if len(evaluation_results) < max_evaluation_sections_number:
+                    evaluation_results += [''] * (max_evaluation_sections_number - len(evaluation_results))
 
-                split_interval = len(user_train_results) / ScoreGeneratorConsts.GRAPH_MARKS_COUNT
-                previous_index = 0
-                for i in range(ScoreGeneratorConsts.GRAPH_MARKS_COUNT):
-                    mean_record_index = (i+1)*split_interval
-                    key_string = '{record_index}_mean'.format(
-                        record_index=mean_record_index
-                    )
-                    if key_string not in split_keys:
-                        split_keys.append(key_string)
-
-                    raw_info_dict[uid][experiment_type][key_string] = \
-                        sum(user_train_results[previous_index:mean_record_index-1]) / float(split_interval)
-                    previous_index = mean_record_index-1
+        evaluation_sections_marks = ['eval_' + str(eval_id) for eval_id in range(max_evaluation_sections_number)]
 
         # write results to csv
         self.__logger.log('writing info to csv file')
-        csv_file_path = os.path.join(os.path.dirname(__file__), 'scores.csv')
+        csv_file_path = os.path.join(results_folder_path, 'scores.csv')
 
-        stats_keys = ['train_mean', 'train_time', 'eval', 'eval_time']
-        stats_keys += split_keys
+        stats_keys = ['train_mean_duration', 'evaluation_mean_duration']
 
         with open(csv_file_path, 'wb') as csv_file:
             writer = csv.writer(csv_file)
 
-            headers = ['user_id', 'experiment_type'] + stats_keys
+            headers = ['user_id', 'experiment_type'] + stats_keys + evaluation_sections_marks
             writer.writerow(headers)
 
         for uid in user_ids:
@@ -259,6 +153,8 @@ class ScoreGenerator:
                 fields = [uid, experiment_type]
                 for key_name in stats_keys:
                     fields.append(raw_info_dict[uid][experiment_type][key_name])
+                for eval_id in range(max_evaluation_sections_number):
+                    fields.append(raw_info_dict[uid][experiment_type]['evaluation_mean_results'][eval_id])
 
                 with open(csv_file_path, 'ab') as csv_file:
                     writer = csv.writer(csv_file)
@@ -268,6 +164,9 @@ class ScoreGenerator:
 
 
 if __name__ == '__main__':
-    logger = Logger()
-    generator = ScoreGenerator(logger)
-    generator.generate_users_score()
+    if len(sys.argv) != 2:
+        print "usage: score_generator.py <results folder path>"
+    else:
+        logger = Logger()
+        generator = ScoreGenerator(logger)
+        generator.generate_users_score(sys.argv[1])
--- score_generator_consts.py
+++
@@ -1,8 +0,0 @@
-
-class ScoreGeneratorConsts:
-
-    EXPERIMENT_TYPE = 'predator'
-
-    RESULTS_FOLDER_NAME = r'result_storage/{ex_type}_results'.format(ex_type=EXPERIMENT_TYPE)
-
-    GRAPH_MARKS_COUNT = 10
